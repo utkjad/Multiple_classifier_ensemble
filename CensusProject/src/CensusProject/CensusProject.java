73bbaf227d9135b9589fcf465d55aad149f7504f
smithabn11 <smithabn@ccs.neu.edu>
2015-04-29 12:39:50 -0400

    Removing Normalisation Part and edited README and output files accordingly.

--- CensusProject/src/CensusProject/CensusProject.java
+++ CensusProject/src/CensusProject/CensusProject.java
@@ -4,7 +4,6 @@ run the Majority Voting algorithm using preprocessing.
  */
 package CensusProject;
 
-import java.io.BufferedReader;
 import java.util.Scanner;
 
 import preprocessing.*;
@@ -28,22 +27,21 @@ public class CensusProject {
 		String dataArfffile = "census-income-data.arff";
 		String testArfffile = "census-income-test.arff";
 
+		/*Convert the given files to arff files*/
 		ConversionToArff objConversionToArff = new ConversionToArff();
 		objConversionToArff.convertToArffFile("census-income.data", dataArfffile);
 		objConversionToArff.convertToArffFile("census-income.test", testArfffile);
 
-		/* Now Read the Arff files to Instances */
 		Preprocessing objPreprocess = new Preprocessing();
 
+		/* Now Read the Arff files to Instances */
 		Instances train = objPreprocess.readArffFile(dataArfffile);
 		Instances test = objPreprocess.readArffFile(testArfffile);
 		
 		System.out.println("Run Options:");
 		System.out.println("1) Enter 1 to run Majority Voting with Replacing Missing Values(Mean & Mode Imputation)");
 		System.out.println("2) Enter 2 to run Majority Voting with Replacing Missing Values(Mean & Mode Imputation) then SMOTE");
-		System.out.println("3) Enter 3 to run Majority Voting with Replacing Missing Values(Mean & Mode Imputation) then SMOTE then Normalisation");
-		System.out.println("4) Enter 4 to run Majority Voting with Removing Missing Values");
-		System.out.println("5) Enter 5 to run Majority Voting without any Preprocessing");
+		System.out.println("3) Enter 3 to run Majority Voting with Removing Missing Values");
 		System.out.println("Enter your choice: ");
 		
 		Scanner in = new Scanner(System.in);
@@ -57,7 +55,7 @@ public class CensusProject {
 			 */
 			MajorityVote mV = new MajorityVote();
 			
-			if (choice==1)/*This is our algorithm*/ {
+			if (choice==1){
 				System.out.println("=== Starting to Run Majority Voting with Replacing Missing Values(Mean & Mode Imputation) ===");
 				/* Replace Missing Values in Data */
 				Instances trainReplaceMissingValues = objPreprocess.replaceMissingValues(train);
@@ -65,11 +63,12 @@ public class CensusProject {
 				Instances testFilledMissingValues = objPreprocess.replaceMissingValues(test);
 				mV.MajorityVotePrediction(trainReplaceMissingValues, testFilledMissingValues);
 			} else if (choice==2) {
-				System.out.println("=== Starting to Run Majority Voting with Replacing Missing Values(Mean & Mode Imputation) then SMOTE ===");
+				/******This is our algorithm******/
+				System.out.println("=== Starting to Run Majority Voting with Replacing Missing Values(Mean & Mode Imputation) then SMOTE percentage=65% ===");
 				/* Replace Missing Values in Data */
 				Instances trainReplaceMissingValues = objPreprocess.replaceMissingValues(train);
 				/* Balance The Data using SMOTE */
-				Instances trainBalancedData = objPreprocess.BalanceDataSMOTE(trainReplaceMissingValues,65, 5);
+				Instances trainBalancedData = objPreprocess.BalanceDataSMOTE(trainReplaceMissingValues,65,5);
 				/* Randomize the Data after using SMOTE */
 				Instances trainBalancedRandomizedData = objPreprocess.AfterSmoteDataRandomize(trainBalancedData);
 				/* Replace Missing Values in Test */
@@ -77,22 +76,6 @@ public class CensusProject {
 				/* Running Majority Voting */
 				mV.MajorityVotePrediction(trainBalancedRandomizedData, testFilledMissingValues);
 			} else if (choice==3) {
-				System.out.println("=== Starting to Run Majority Voting with Replacing Missing Values(Mean & Mode Imputation) then SMOTE then Normalisation ===");
-				/* Replace Missing Values in Train */
-				Instances trainReplaceMissingValues = objPreprocess.replaceMissingValues(train);
-				/* Balance The Data using SMOTE */
-				Instances trainBalancedData = objPreprocess.BalanceDataSMOTE(trainReplaceMissingValues, 65, 5);
-				/* Randomize the Data after using SMOTE */
-				Instances trainBalancedRandomizedData = objPreprocess.AfterSmoteDataRandomize(trainBalancedData);
-				/* Normalize the Train Data */
-				Instances normalizedTrainData = objPreprocess.NormalizedData(trainBalancedRandomizedData);
-				/* Replace Missing Values in Test */
-				Instances testFilledMissingValues = objPreprocess.replaceMissingValues(test);
-				/* Normalize the Test Data */
-				Instances normalizedTestData = objPreprocess.NormalizedData(testFilledMissingValues);
-				/* Running Majority Voting */
-				mV.MajorityVotePrediction(normalizedTrainData, normalizedTestData);
-			} else if (choice==4) {
 				System.out.println("=== Starting to Run Majority Voting with Removing Missing Values ===");
 				/* Removing Missing Values From Train */
 				Instances trainRemoveMissingValues = objPreprocess.removeMissingInstances(train);
@@ -100,14 +83,10 @@ public class CensusProject {
 				Instances testRemoveMissingValues = objPreprocess.removeMissingInstances(test);
 				/* Running Majority Voting */
 				mV.MajorityVotePrediction(trainRemoveMissingValues, testRemoveMissingValues);
-			} else if(choice==5){
-				System.out.println("=== Starting to Run Majority Voting without any Preprocessing ===");
-				mV.MajorityVotePrediction(train, test);
-			}
-			else{
+			} else{
 				System.out.println("Invalid Choice, run the project again");
 				System.exit(0);
-				}
+			}
 		}
 	}
 }
--- README.txt
+++ README.txt
@@ -31,7 +31,14 @@ How to compile and run :
 If weka.jar is coming as not found follow the steps to add weka.jar to
 this project: http://www.wikihow.com/Add-JARs-to-Project-Build-Paths-in-Eclipse-%28Java%29
 Follow Method 1.
-3)Copy the data files(census-income.data and census-income.test) into CensusProject folder.
-4)Run the project. (NOTE: The time take to get output : 120 secs approx)
+3)Copy the data files(census-income.data and census-income.test) into CensusProject folder.(NOTE: By default already present)
+4)Run the project (i.e.CensusProject.java (our main program)). (NOTE: The time take to get output : 100~180 secs approx)
+5)You will asked to key in the choice for Run Options on console. Please enter number 2.(Our algorithm)
+(NOTE: Output after running other options discussed in report. Same can be tested in our program by choosing respective options)
 
-Output: The output is generated on console. The output.txt file contains the output for various options selected during runtime.
+Output: The output is generated on console. 
+NOTE: census-income-data.arff and census-income-test.arff are created from census-income.data
+census-income.test and  our project will read data from arff files.
+
+Just For Your Reference:
+The output.txt file contains the output for various options selected during runtime.
